70f006a17267903799b629b14648225932b3f51d
Pro100-Almaz <almaz.amanzholuly@nu.edu.kz>
2023-04-15 19:16:30 +0600

    Added: template for requests list and item

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -3,14 +3,21 @@
     <p class="subhead-topic">Информация о пользователе</p>
     <hr />
     <div>
-      <InputText disabled v-model="nameOfUser" class="input-field" />
+      <InputText disabled v-model="iin" class="input-field iin" />
+      <div class="user-name">
+        <InputText disabled v-model="nameOfUser" class="input-field name" />
+        <InputText disabled v-model="surnameOfUser" class="input-field name" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+
+const iin = ref("020214500327");
 const nameOfUser = ref("Almaz");
+const surnameOfUser = ref("Amanzholuly");
 </script>
 
 <style scoped>
@@ -18,5 +25,21 @@ const nameOfUser = ref("Almaz");
   padding: 1rem;
   border-radius: 25px;
   background-color: var(--secondary-background);
+}
+
+.iin {
+  width: 100%;
+  margin-bottom: 1.5rem;
+}
+
+.name {
+  width: 100%;
+  margin-bottom: 1rem;
+}
+
+.user-name {
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
 }
 </style>
