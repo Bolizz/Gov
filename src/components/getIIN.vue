f464749997038bd8f2d1050dadd23cade7f2bfde
kanat <abdsattar02@inbox.ru>
2023-04-16 01:39:51 +0600

    feature(get data)

---
+++ src/components/getIIN.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <InputNumber v-model="value" inputId="IIN" />
+    <Button label="Submit" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import InputNumber from 'primevue/inputnumber';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+
+const value = ref(Number);
+</script>
+
+<style>
+.app{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DocumentComfirmationVue from "@/views/DocumentComfirmation.vue";
 import headerOfMainPageVue from "@/views/headerOfMainPage.vue";
+import getIIN from "@/components/getIIN.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,11 @@ const router = createRouter({
       path: "/home",
       name: "header",
       component: headerOfMainPageVue,
+    },
+    {
+      path: "/getIIN",
+      name: "data",
+      component: getIIN,
     },
   ],
 });
