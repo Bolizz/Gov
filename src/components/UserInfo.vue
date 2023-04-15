b8301b840300d588469ebe4668f7ab7a83fc708a
Pro100-Almaz <almaz.amanzholuly@nu.edu.kz>
2023-04-15 15:38:25 +0600

    Fixed: structure of comfirmation form page

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-info">
+    <p class="subhead-topic">Информация о пользователе</p>
+    <div>
+      <InputText disabled v-bind:placeholder="nameOfUser" class="input-field" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const nameOfUser = ref("Almaz");
+</script>
+
+<style scoped>
+.user-info {
+  padding: 1rem;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
+import InputText from "primevue/inputtext";
+
+import "./assets/base.css";
+
 const app = createApp(App);
+
+app.component("InputText", InputText);
 
 app.use(createPinia());
 app.use(router);
